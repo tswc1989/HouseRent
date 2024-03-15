f5541b25c5cc9748a520bdd4e59e67129aab2e21
zhangjie <tianshenwocai09@aliyun.com>
2024-03-15 14:49:02 +0800

    重构

---
+++ src/main/java/com/house/rent/service/impl/OrderServiceImpl.java
@@ -0,0 +1,39 @@
+package com.house.rent.service.impl;
+
+import java.util.List;
+
+import com.house.rent.dao.OrderMapper;
+import com.house.rent.entity.Order;
+import com.house.rent.entity.Page;
+import com.house.rent.entity.UserOrder;
+import com.house.rent.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements IOrderService {
+
+	@Autowired
+	private OrderMapper mapper;
+	
+	@Override
+	public int addOrder(Order order) {
+		return mapper.addOrder(order);
+	}
+
+	@Override
+	public List<UserOrder> findAllOrder(Page page) {
+		return mapper.findAllOrder(page);
+	}
+
+	@Override
+	public int getOrderCount(int uID) {
+		return mapper.getOrderCount(uID);
+	}
+
+	@Override
+	public int deleteOrder(int oID) {
+		return mapper.deleteOrder(oID);
+	}
+
+}
